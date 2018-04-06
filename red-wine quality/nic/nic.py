27a9b4af71351530c9a05cbf814f7799fa49a2b8
nicolalandro <nicolaxx94@live.it>
2018-04-06 14:49:53 +0200

    nic folder with class test

--- red-wine quality/nic/nic.py
+++ red-wine quality/nic/nic.py
@@ -7,3 +7,17 @@ names_without_quality = ['fixed acidity', 'volatile acidity', 'citric acid', 're
                          'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
 dataset = pd.read_csv('data/winequality-red.csv', names=names)
 np.savetxt('test.txt', dataset.corr())
+
+class_3 = dataset[dataset['quality'] == 3]
+class_4 = dataset[dataset['quality'] == 4]
+class_5 = dataset[dataset['quality'] == 5]
+class_6 = dataset[dataset['quality'] == 6]
+class_7 = dataset[dataset['quality'] == 7]
+class_8 = dataset[dataset['quality'] == 8]
+
+np.savetxt('class3.txt', class_3.corr())
+np.savetxt('class4.txt', class_4.corr())
+np.savetxt('class5.txt', class_5.corr())
+np.savetxt('class6.txt', class_6.corr())
+np.savetxt('class7.txt', class_7.corr())
+np.savetxt('class8.txt', class_8.corr())
