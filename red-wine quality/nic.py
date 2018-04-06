642d724d1db338c1b1e21f59375e7d7353a10d17
nicolalandro <nicolaxx94@live.it>
2018-04-06 14:36:00 +0200

    * test

---
+++ red-wine quality/nic.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+names = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide',
+         'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
+names_without_quality = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
+                         'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
+dataset = pd.read_csv('data/winequality-red.csv', names=names)
+np.savetxt('test.txt', dataset.corr())
