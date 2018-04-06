fc32e0bc017a913514406330e79c1342b0dab6e4
nicolalandro <nicolaxx94@live.it>
2018-04-06 15:09:21 +0200

    correlazione tra feature della classe

--- red-wine quality/nic/nic.py
+++ red-wine quality/nic/nic.py
@@ -6,7 +6,7 @@ names = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', '
 names_without_quality = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
                          'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
 dataset = pd.read_csv('data/winequality-red.csv', names=names)
-np.savetxt('test.txt', dataset.corr())
+np.savetxt('nic/test.txt', dataset.corr())
 
 class_3 = dataset[dataset['quality'] == 3]
 class_4 = dataset[dataset['quality'] == 4]
@@ -15,9 +15,16 @@ class_6 = dataset[dataset['quality'] == 6]
 class_7 = dataset[dataset['quality'] == 7]
 class_8 = dataset[dataset['quality'] == 8]
 
-np.savetxt('class3.txt', class_3.corr())
-np.savetxt('class4.txt', class_4.corr())
-np.savetxt('class5.txt', class_5.corr())
-np.savetxt('class6.txt', class_6.corr())
-np.savetxt('class7.txt', class_7.corr())
-np.savetxt('class8.txt', class_8.corr())
+np.savetxt('nic/class3.txt', class_3.corr())
+np.savetxt('nic/class4.txt', class_4.corr())
+np.savetxt('nic/class5.txt', class_5.corr())
+np.savetxt('nic/class6.txt', class_6.corr())
+np.savetxt('nic/class7.txt', class_7.corr())
+np.savetxt('nic/class8.txt', class_8.corr())
+
+np.savetxt('nic/corr_coef3.txt', np.corrcoef(class_3))
+np.savetxt('nic/corr_coef4.txt', np.corrcoef(class_4))
+np.savetxt('nic/corr_coef5.txt', np.corrcoef(class_5))
+np.savetxt('nic/corr_coef6.txt', np.corrcoef(class_6))
+np.savetxt('nic/corr_coef7.txt', np.corrcoef(class_7))
+np.savetxt('nic/corr_coef8.txt', np.corrcoef(class_8))
