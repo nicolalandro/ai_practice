f85ce0f7b76012de0df14d70e503bad2d6f4c726
nicolalandro <nicolaxx94@live.it>
2018-04-06 17:05:44 +0200

    svc performance

--- red-wine quality/nic/main.py
+++ red-wine quality/nic/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 
 names = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide',
          'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
@@ -17,4 +18,10 @@ train_examples, test_examples, train_truths, test_truths = train_test_split(exam
 classifier = KNeighborsClassifier(n_neighbors=3)
 classifier.fit(train_examples, train_truths)
 prediction = classifier.predict(test_examples)
-print("result:", accuracy_score(test_truths, prediction))
+print("knn result:", accuracy_score(test_truths, prediction))
+
+classifier = SVC()
+classifier.fit(train_examples, train_truths)
+prediction = classifier.predict(test_examples)
+print("svn result:", accuracy_score(test_truths, prediction))
+
