f25b6ea120ddce4873626b61a58bcc6a43388c54
nicolalandro <nicolaxx94@live.it>
2018-04-06 17:01:48 +0200

    nic main

---
+++ red-wine quality/nic/main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+names = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide',
+         'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
+names_without_quality = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides',
+                         'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
+dataset = pd.read_csv('data/winequality-red.csv', names=names)
+
+examples = dataset.drop(['quality'], axis=1).values
+truths = dataset['quality'].get_values()
+
+train_examples, test_examples, train_truths, test_truths = train_test_split(examples, truths, test_size=0.33)
+
+classifier = KNeighborsClassifier(n_neighbors=3)
+classifier.fit(train_examples, train_truths)
+prediction = classifier.predict(test_examples)
+print("result:", accuracy_score(test_truths, prediction))
