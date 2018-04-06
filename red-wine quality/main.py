cfa7a80b09ed95c965fcd1c5c237a41300c1b425
luca-landa <lucalanda@hotmail.it>
2018-04-06 14:08:50 +0200

    * VINOOH

---
+++ red-wine quality/main.py
@@ -0,0 +1,33 @@
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
+import numpy as np
+import pandas as pd
+
+names = ['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol','quality']
+names_without_quality = ['fixed acidity','volatile acidity','citric acid','residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide','density','pH','sulphates','alcohol']
+dataset = pd.read_csv('data/winequality-red.csv', names=names)
+
+examples = []
+truths = []
+
+for index, row in dataset.iterrows():
+    example = []
+    for name in names_without_quality:
+        example.append(float(row[name]))
+    examples.append(example)
+    truths.append(row['quality'])
+
+train_examples, test_examples, train_truths, test_truths = train_test_split(examples, truths, test_size=0.33)
+
+classifier = KNeighborsClassifier(n_neighbors=3)
+classifier.fit(train_examples, train_truths)
+prediction = classifier.predict(test_examples)
+# print("result:", accuracy_score(test_truths, prediction))
+corrcoef = np.corrcoef(examples)
+for row in corrcoef:
+    if np.amin(row) <= 0.6:
+        #TODO scoprire come trovare l'indice dell'elemento dal numpy.ndarray
+        print(row.index(np.amin(row)))
--- start/main_makeup.py
+++ start/main_makeup.py
@@ -6,47 +6,50 @@ truths = []
 
 dataset = pd.read_csv('makeup_dataset.csv', sep='|')
 for index, row in dataset.iterrows():
-	examples.append(str(row[1]) + ' ' + str(row[2]))
-	truths.append(str(row[4]))
+    examples.append(str(row[1]) + ' ' + str(row[2]))
+    truths.append(str(row[4]))
 
 dictionary = CountVectorizer().fit(examples)
 examples = dictionary.transform(examples)
 
 from sklearn.model_selection import train_test_split
+
 train_examples, test_examples, train_truths, test_truths = train_test_split(examples, truths, test_size=0.33)
 
 from sklearn.tree import DecisionTreeClassifier
+
 decision_tree = DecisionTreeClassifier()
 decision_tree.fit(train_examples, train_truths)
 prediction = decision_tree.predict(test_examples)
 
 from sklearn.metrics import accuracy_score
+
 print("result with decision tree:", accuracy_score(test_truths, prediction))
 
 test = 'ombretto blush cipria rossetto'
 transformed_pattern = dictionary.transform([test])
 print(decision_tree.predict(transformed_pattern))
 
-
 from sklearn.neighbors import KNeighborsClassifier
+
 decision_tree = KNeighborsClassifier()
 decision_tree.fit(train_examples, train_truths)
 prediction = decision_tree.predict(test_examples)
 
-#from sklearn.metrics import accuracy_score
+# from sklearn.metrics import accuracy_score
 print("result with decision tree:", accuracy_score(test_truths, prediction))
 
 test = 'ombretto blush cipria rossetto'
 transformed_pattern = dictionary.transform([test])
 print(decision_tree.predict(transformed_pattern))
 
-
 from sklearn.ensemble import ExtraTreesClassifier
+
 decision_tree = ExtraTreesClassifier(n_estimators=100, n_jobs=12, bootstrap=False, min_samples_split=2, random_state=0)
 decision_tree.fit(train_examples, train_truths)
 prediction = decision_tree.predict(test_examples)
 
-#from sklearn.metrics import accuracy_score
+# from sklearn.metrics import accuracy_score
 print("result with decision tree:", accuracy_score(test_truths, prediction))
 
 test = 'ombretto blush cipria rossetto'
