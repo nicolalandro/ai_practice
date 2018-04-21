d63b70eb7395a6fa7df4c44e7e9ab1f7b29ada02
nicolalandro <nicolaxx94@live.it>
2018-04-21 20:35:48 +0200

    accuracy score sulle iris con sklearn

--- 03_keras/nic.py
+++ 03_keras/nic.py
@@ -21,21 +21,23 @@ from keras.models import Sequential
 model = Sequential()
 from keras.layers import Dense
 
-model.add(Dense(units=64, activation='relu', input_dim=4))
-model.add(Dense(units=64, activation='sigmoid', input_dim=4))
+# model.add(Dense(units=64, activation='relu', input_dim=4))
+# model.add(Dense(units=64, activation='sigmoid', input_dim=4))
+# model.add(Dense(units=3, activation='softmax'))
+model.add(Dense(units=16, activation='relu', input_dim=4))
 model.add(Dense(units=3, activation='softmax'))
 
 model.compile(loss=keras.losses.sparse_categorical_crossentropy,
               optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
 
-model.fit(x_train, y_train, epochs=4, batch_size=32)
+model.fit(x_train, y_train, epochs=20, batch_size=32)
 
 loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
-print(loss_and_metrics)
+print("loss and metrics: ", loss_and_metrics)
 
 prediction = model.predict(x_test, batch_size=128)
-print(prediction)
-# import numpy as np
-# test_truth = np.asarray(map(class_number_to_array, y_test))
-# print(type(test_truth))
-# print(type(prediction))
+prediction = prediction.argmax(1)
+
+from sklearn.metrics import accuracy_score
+
+print("accuracy score: ", accuracy_score(y_test, prediction))
