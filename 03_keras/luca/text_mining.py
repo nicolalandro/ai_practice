6b360214aec735dcf822709a5892390d5f839881
nicolalandro <nicolaxx94@live.it>
2018-05-11 13:41:17 +0200

    refactor con pipeline

--- 03_keras/luca/text_mining.py
+++ 03_keras/luca/text_mining.py
@@ -1,28 +1,22 @@
+import numpy as np
 from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
 twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
 
-from sklearn.feature_extraction.text import CountVectorizer
-count_vect = CountVectorizer()
-dictionary = count_vect.fit(twenty_train.data)
-X_train_counts = dictionary.transform(twenty_train.data)
-# X_train_counts.shape
+clf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('multinomial NB', MultinomialNB())
+])
 
-from sklearn.feature_extraction.text import TfidfTransformer
-tfidf_transformer = TfidfTransformer()
-tfidf_clf = tfidf_transformer.fit(X_train_counts)
-X_train_tfidf = tfidf_clf.transform(X_train_counts)
-# X_train_tfidf.shape
+clf.fit(twenty_train.data, twenty_train.target)
 
-from sklearn.naive_bayes import MultinomialNB
-clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
-
-import numpy as np
 twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
-test_data = dictionary.transform(twenty_test.data)
-
-
-predicted = clf.predict(tfidf_clf.transform(test_data))
+predicted = clf.predict(twenty_test.data)
 print(np.mean(predicted == twenty_test.target))
-
-from sklearn.metrics import accuracy_score
 print(accuracy_score(twenty_test.target, predicted))
