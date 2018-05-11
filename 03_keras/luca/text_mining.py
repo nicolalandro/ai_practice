3ecc02b2e06d761a0632814f95f4eab6a45300dd
nicolalandro <nicolaxx94@live.it>
2018-05-11 14:01:40 +0200

    gli altri fanno piu schifo

--- 03_keras/luca/text_mining.py
+++ 03_keras/luca/text_mining.py
@@ -1,12 +1,12 @@
-import numpy as np
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics import accuracy_score
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn.pipeline import Pipeline
 
 twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
+twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
 
 clf = Pipeline([
     ('vect', CountVectorizer()),
@@ -16,7 +16,17 @@ clf = Pipeline([
 
 clf.fit(twenty_train.data, twenty_train.target)
 
-twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
 predicted = clf.predict(twenty_test.data)
-print(np.mean(predicted == twenty_test.target))
+# print(np.mean(predicted == twenty_test.target))
 print(accuracy_score(twenty_test.target, predicted))
+
+clf2 = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', GaussianNB())
+])
+
+clf2.fit(twenty_train.data, twenty_train.target)
+
+predicted2 = clf2.predict(twenty_test.data)
+print(accuracy_score(twenty_test.target, predicted2))
